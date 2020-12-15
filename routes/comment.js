4d1447b0d28d4dafee01e52027e5159248d164b7
Le Anh Huy <vrinv123456@gmail.com>
2020-12-15 17:10:40 +0700

    comment

--- routes/comment.js
+++ routes/comment.js
@@ -2,7 +2,7 @@ const express = require('express')
 // const router = express.Router()
 const router = require('express-promise-router')()
 
-const CommentController = require('../controllers/Comment')
+const CommentController = require('../controllers/comment')
 
 const { validateBody, validateParam, schemas } = require('../helpers/routerHelpers')
 
